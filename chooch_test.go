618546d26b5e58fe2f39feeba7b42b09a81f03cd
Wes Johnson <wes@futureprefect.com>
2016-10-06 21:18:39 -0700

    Improve TestPing, add TestHtoI
    TestPing now creates a host struct, instead of depending on readhosts().

--- chooch_test.go
+++ chooch_test.go
@@ -5,6 +5,7 @@ import "testing"
 var filetests = []string{
 	"http://localhost:6060/",
 	"localhost",
+	"www.google.com",
 }
 
 func TestReadHosts(t *testing.T) {
@@ -13,10 +14,29 @@ func TestReadHosts(t *testing.T) {
 		if hosts[i].name != f {
 			t.Errorf("hosts[%d]: got host{name: %s}, want host{name: %s}", i, hosts[i].name, f)
 		}
-
 	}
 }
 
 func TestPing(t *testing.T) {
-	hosts[1].ping()
+	// TODO: Improve this when ping() has output.
+	s := make([]string, 0)
+	addrs := append(s, "127.0.0.1")
+	host{name: "localhost", addrs: addrs}.ping()
+
+	//s := make([]string, 0)
+	//addrs := append(s, "127.0.0.1")
+	host{name: "localhost", addrs: addrs}.ping()
+}
+
+func TestHtoI(t *testing.T) {
+	h := host{name: "localhost"}
+	want := "127.0.0.1"
+	err := h.htoi()
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := h.addrs[0]
+	if got != want {
+		t.Errorf("h.addrs: got %s, want %s", got, want)
+	}
 }
