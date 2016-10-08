dcbe04bc6dafcc4c67bdb1a126e31145443e4211
Wes Johnson <wes@steelhouse.com>
2016-10-08 00:47:15 -0700

    Add init(), improve TestPing()
    init() now bootstaps a host{} that all tests can use.
    TestPing() reasonably tests ping(), and follow on method addResp().

--- chooch_test.go
+++ chooch_test.go
@@ -1,11 +1,21 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+var h host
 
 var filetests = []string{
 	"http://localhost:6060/",
 	"localhost",
-	"www.google.com",
+}
+
+func init() {
+	h = host{name: "localhost"}
+	s := make([]string, 0)
+	h.addrs = append(s, "127.0.0.1")
 }
 
 func TestReadHosts(t *testing.T) {
@@ -19,9 +29,29 @@ func TestReadHosts(t *testing.T) {
 
 func TestPing(t *testing.T) {
 	// TODO: Improve this when ping() has output.
-	s := make([]string, 0)
-	addrs := append(s, "127.0.0.1")
-	host{name: "localhost", addrs: addrs}.ping()
+	want := resp{
+		seq:  0,
+		code: 0,
+		to:   "127.0.0.1",
+		from: "127.0.0.1",
+		body: fmt.Sprintf("Ranger-Chooch-%s", h.name),
+	}
+	h.ping()
+	if h.resps[0].seq != want.seq {
+		t.Errorf("h.resps[0].seq got %d, want %d", h.resps[0].seq, want.seq)
+	}
+	if h.resps[0].code != want.code {
+		t.Errorf("h.resps[0].code got %d, want %d", h.resps[0].code, want.code)
+	}
+	if h.resps[0].to != want.to {
+		t.Errorf("h.resps[0].to got %s, want %s", h.resps[0].to, want.to)
+	}
+	if h.resps[0].from != want.from {
+		t.Errorf("h.resps[0].from got %s, want %s", h.resps[0].from, want.from)
+	}
+	if h.resps[0].body != want.body {
+		t.Errorf("h.resps[0].body got %s, want %s", h.resps[0].body, want.body)
+	}
 }
 
 func TestHtoI(t *testing.T) {
@@ -35,4 +65,8 @@ func TestHtoI(t *testing.T) {
 	if got != want {
 		t.Errorf("h.addrs: got %s, want %s", got, want)
 	}
+}
+
+func TestAddResp(t *testing.T) {
+
 }
