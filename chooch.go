704ac7ddca143034ceb00bac9617a9ebdf1f59ce
Wes Johnson <wes@futureprefect.com>
2016-10-15 15:33:34 -0700

    Replace TODO with #2

--- chooch.go
+++ chooch.go
@@ -198,7 +198,6 @@ func (h *host) addResp(id, seq, code int, sent, recv time.Time, dur time.Duratio
 func main() {
 	// if an entry is a url, send a GET request
 	// if an entry is a hostname, send an ICMP ping
-	// TODO: figure out how to flush h.resps.resp{}.
 	// TODO: store responses in google sheets.
 	// TODO: cache writes to google sheets if network is unavailable.
 	// TODO: rewrite host request methods as goroutines.
